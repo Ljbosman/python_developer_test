cbe223d7b23f322793760bc456cf18e8a38dcf11
Llewellyn Bosman <llewellyn.bosman@takealot.com>
2020-07-01 09:28:27 +0200

    Fix demo.sh

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -1,3 +1,3 @@
-python3 csv_reader/csv_reader.py -i file.csv -s -l 25 -t -d "01 Jun 1999" "31 Aug 2007"
+python3 csv_reader/csv_reader.py -i csv_reader/file.csv -s -l 25 -t -d "01 Jun 1999" "31 Aug 2007"
 
 
